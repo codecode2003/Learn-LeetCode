507d32a13bb2e334a73ed0019c1cd8dbe83af0eb
Khánh Duy <learncodecode2003@gmail.com>
2025-04-21 19:26:50 +0700

    1071. Greatest Common Divisor of Strings

---
+++ Java/1001-1100/1071. Greatest Common Divisor of Strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1+str2).equals(str2+str1)) return "";
+        int n = gcd(str1.length(), str2.length());
+        return str1.substring(0, n);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+}
