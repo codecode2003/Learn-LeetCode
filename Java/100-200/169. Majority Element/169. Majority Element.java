36c53ef4e2fb50d29823c78b8d50e32eb98f6642
Khánh Duy <learncodecode2003@gmail.com>
2025-04-22 21:17:43 +0700

    169. Majority Element

---
+++ Java/100-200/169. Majority Element/169. Majority Element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > nums.length / 2) return num;
+        }
+        return 0;
+    }
+}
